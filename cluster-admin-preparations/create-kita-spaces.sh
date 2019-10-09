781900deaa9abe08a2fed3f63162c8a9f6bb3425
Daniel Strebel <daniel.strebel@gmail.com>
2019-10-09 07:44:48 +0200

    zero padding user names

--- cluster-admin-preparations/create-kita-spaces.sh
+++ cluster-admin-preparations/create-kita-spaces.sh
@@ -1,7 +1,8 @@
 # Provide the number of users as argument
 END=$1
 
-for ((i=1;i<=END;i++)); do
+for i in $(seq -f "%03g" 1 $END)
+do
     access_token=viscon-$(openssl rand -hex 3)
     space=$(sed "s/ACCESS_TOKEN/$access_token/" cluster-admin-preparations/viscon-kita-workspace.yaml)
     space=$(echo "$space" | sed "s/USER_ID/viscon-$i/")
